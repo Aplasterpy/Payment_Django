2bb4583198cb4bf84ea9809200d8c3d91a8c8ab6
Sukalyan <53527956+Aplasterpy@users.noreply.github.com>
2022-02-17 01:15:55 +0530

    Update settings.py

--- payment/settings.py
+++ payment/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5@20mlfu24ykou#52d-&ikpat$0g7ji9-sv-9_u#ar0av@p0bb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
